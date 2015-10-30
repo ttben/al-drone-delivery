b51b4271c3123ad3e0e88ea6e0a877cd0ac0f1e5
Tom GUILLERMIN <tom.guillermin@gmail.com>
2015-10-30 08:46:24 +0100

    Basic simulation process: added fake orders

--- src/main/java/fr/unice/polytech/si5/al/projet/s3/central/Location.java
+++ src/main/java/fr/unice/polytech/si5/al/projet/s3/central/Location.java
@@ -9,4 +9,15 @@ public class Location {
 	private String zipOrPostalCode;
 	private String country;
 
+	public Location(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 }
--- src/main/java/fr/unice/polytech/si5/al/projet/s3/simulation/Main.java
+++ src/main/java/fr/unice/polytech/si5/al/projet/s3/simulation/Main.java
@@ -1,12 +1,19 @@
 package fr.unice.polytech.si5.al.projet.s3.simulation;
 
 import fr.unice.polytech.si5.al.projet.s3.central.CentralWarehouse;
+import fr.unice.polytech.si5.al.projet.s3.central.Item;
+import fr.unice.polytech.si5.al.projet.s3.central.Location;
+import fr.unice.polytech.si5.al.projet.s3.central.Order;
 import fr.unice.polytech.si5.al.projet.s3.truck.DroneDeliveryApp;
 import fr.unice.polytech.si5.al.projet.s3.truck.Truck;
 import fr.unice.polytech.si5.al.projet.s3.truck.TruckDevice;
 import fr.unice.polytech.si5.al.projet.s3.truck.TruckDriver;
 import fr.unice.polytech.si5.al.projet.s3.warehouse.SimpleWarehouse;
 import fr.unice.polytech.si5.al.projet.s3.warehouse.Warehouse;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  *
@@ -32,6 +39,8 @@ public class Main {
 		Truck t3 = initializeTruck(w2);
 
 		TruckDriver t1d = t1.getDriver();
+
+		List<Order> orders = createOrders();
 	}
 
 	public Truck initializeTruck(Warehouse w) {
@@ -51,4 +60,23 @@ public class Main {
 		return truck;
 	}
 
+	public List<Order> createOrders() {
+		List<Order> orders = new LinkedList<Order>();
+
+		Item i1 = new Item("item1");
+		Item i2 = new Item("item2");
+		Item i3 = new Item("item3");
+		Item i4 = new Item("item4");
+		Item i5 = new Item("item5");
+		Location location1 = new Location("derpboy");
+		Order o1 = new Order(new LinkedList<Item>(Arrays.asList(i1, i2, i3)), location1);
+
+		Location location2 = new Location("derpina");
+		Order o2 = new Order(new LinkedList<Item>(Arrays.asList(i4, i5)), location2);
+
+		orders.add(o1);
+		orders.add(o2);
+
+		return orders;
+	}
 }
