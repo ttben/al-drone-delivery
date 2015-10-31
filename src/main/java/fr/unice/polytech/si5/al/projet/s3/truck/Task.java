d1dedd841e1b79739f64a50e7952d49df0d92fa8
Benjamin Benni <benjamin.benni06@gmail.com>
2015-10-31 08:39:31 +0100

    Task become an interface

--- src/main/java/fr/unice/polytech/si5/al/projet/s3/truck/Task.java
+++ src/main/java/fr/unice/polytech/si5/al/projet/s3/truck/Task.java
@@ -3,18 +3,10 @@ package fr.unice.polytech.si5.al.projet.s3.truck;
 import fr.unice.polytech.si5.al.projet.s3.drone.Drone;
 import fr.unice.polytech.si5.al.projet.s3.shipping.Shipping;
 
-public class Task {
-	private Drone drone;
-	private Shipping shipping;
-	private TaskStatus taskStatus;
+import java.util.List;
 
-	public Task(Drone drone, Shipping shipping) {
-		this.drone = drone;
-		this.shipping = shipping;
-		this.taskStatus = TaskStatus.PENDING;
-	}
-
-	public String getTaskDescription() {
-		return "Prendre le drone "  + drone.getName() + " et le paquet ";
-	}
+public interface Task {
+	boolean isDone();
+	void execute();
+	List<Task> develop();
 }
