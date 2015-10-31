416711f0dcd9c325c7630a06357359dcd237811c
Benjamin Benni <benjamin.benni06@gmail.com>
2015-10-31 07:37:56 +0100

    Add Status classes

--- src/main/java/fr/unice/polytech/si5/al/projet/s3/truck/Task.java
+++ src/main/java/fr/unice/polytech/si5/al/projet/s3/truck/Task.java
@@ -6,10 +6,12 @@ import fr.unice.polytech.si5.al.projet.s3.shipping.Shipping;
 public class Task {
 	private Drone drone;
 	private Shipping shipping;
+	private TaskStatus taskStatus;
 
 	public Task(Drone drone, Shipping shipping) {
 		this.drone = drone;
 		this.shipping = shipping;
+		this.taskStatus = TaskStatus.PENDING;
 	}
 
 	public String getTaskDescription() {
---
+++ src/main/java/fr/unice/polytech/si5/al/projet/s3/truck/TourState.java
@@ -0,0 +1,14 @@
+package fr.unice.polytech.si5.al.projet.s3.truck;
+
+/**
+ * Created by Benjamin on 30/10/2015.
+ */
+public class TourState {
+	private enum STATE {
+		PENDING,
+		DOING,
+		DONE
+	}
+
+
+}
