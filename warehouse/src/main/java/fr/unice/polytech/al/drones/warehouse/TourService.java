c0df9b5bc58813c258e365dde3beeb4796142dea
Sebastien <petillon.sebastien@gmail.com>
2015-11-06 09:37:20 +0100

    Interfaces OK

--- warehouse/src/main/java/fr/unice/polytech/al/drones/warehouse/TourService.java
+++ warehouse/src/main/java/fr/unice/polytech/al/drones/warehouse/TourService.java
@@ -1,4 +1,6 @@
 package fr.unice.polytech.al.drones.warehouse;
+import org.json.JSONObject;
+
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -11,9 +13,28 @@ import javax.ws.rs.core.Response;
 public interface TourService {
 
     @GET
-    Response getTour();
+    @Consumes(MediaType.APPLICATION_JSON)
+    Response getTour(JSONObject description);
 
     @POST
     Response newTour();
+
+    @Path("{id}/status")
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    Response setStatus(@PathParam("id") String id);
+
+    @Path("{id}/delivery/{deliveryId}/status")
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    Response setDeliveryStatus(@PathParam("id") String id,
+                               @PathParam("deliveryId") String deliveryId);
+
+    @Path("{id}/delivery/{deliveryId}/package/{packageId}")
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    Response setPackageDeliveryStatus(@PathParam("id") String id,
+                                      @PathParam("deliveryId") String deliveryId,
+                                      @PathParam("packageId") String packageId);
 
 }
--- warehouse/src/main/java/fr/unice/polytech/al/drones/warehouse/TourServiceImpl.java
+++ warehouse/src/main/java/fr/unice/polytech/al/drones/warehouse/TourServiceImpl.java
@@ -1,5 +1,10 @@
 package fr.unice.polytech.al.drones.warehouse;
 
+import org.json.JSONObject;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 /**
@@ -7,11 +12,23 @@ import javax.ws.rs.core.Response;
  */
 public class TourServiceImpl implements TourService {
 
-    public Response getTour() {
+    public Response getTour(JSONObject description) {
         return Response.ok().build();
     }
 
     public Response newTour() {
+        return Response.ok().build();
+    }
+
+    public Response setStatus(String id) {
+        return Response.ok().build();
+    }
+
+    public Response setDeliveryStatus(String id, String deliveryId) {
+        return Response.ok().build();
+    }
+
+    public Response setPackageDeliveryStatus(String id, String deliveryId, String packageId) {
         return Response.ok().build();
     }
 }
