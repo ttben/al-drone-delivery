26813116a32322e4e3b47ff508c0f972f28d3435
Tom GUILLERMIN <tom.guillermin@gmail.com>
2016-01-23 10:52:34 +0100

    Fixed null target name.

--- newTruckApp/src/main/java/app/Drone.java
+++ newTruckApp/src/main/java/app/Drone.java
@@ -2,19 +2,10 @@ package app;
 
 import app.shipper.BasicShipper;
 
-/**
- * Created by Benjamin on 16/01/2016.
- */
 public class Drone extends BasicShipper {
-
-	private String name;
 
 	public Drone(String name) {
 		super(name);
-	}
-
-	public String getName() {
-		return name;
 	}
 
 	@Override
