1fb091435e840cbed3ccb7431a34f33d123c8292
Benjamin Benni <benjamin.benni06@gmail.com>
2016-02-03 19:28:59 +0100

    Add basic code into new Spy

--- newTruckApp/src/main/java/app/ShippingTrackerObserver.java
+++ newTruckApp/src/main/java/app/ShippingTrackerObserver.java
@@ -1,5 +1,12 @@
 package app;
 
+import app.action.Action;
+import app.action.ActionEvent;
+import app.action.Drop;
+import app.shipper.BasicShipper;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
 
@@ -7,8 +14,17 @@ import java.util.Observer;
  * Created by Benjamin on 03/02/2016.
  */
 public class ShippingTrackerObserver implements Observer {
+	List<String> achievedDeliveries = new ArrayList<>();
+
 	@Override
 	public void update(Observable o, Object arg) {
+		ActionEvent actionEvent = (ActionEvent) arg;
 
+		if(o instanceof Drop && actionEvent.equals(ActionEvent.ENDED)) {
+
+			Drop action = (Drop) o;
+			System.out.printf("\n\t[DELIVERY DONE] %s has performed package %s\n", action.getTarget().getName(), action.getTarget().getPack());
+			
+		}
 	}
 }
