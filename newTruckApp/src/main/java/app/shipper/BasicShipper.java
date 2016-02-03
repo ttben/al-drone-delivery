a6307e67dd877858d13320c9bd35577ea8990cde
Benjamin Benni <benjamin.benni06@gmail.com>
2016-02-03 18:28:28 +0100

    Add package and location into BasicShipper

--- newTruckApp/src/main/java/app/shipper/BasicShipper.java
+++ newTruckApp/src/main/java/app/shipper/BasicShipper.java
@@ -3,11 +3,30 @@ package app.shipper;
 import app.output.Output;
 import app.action.BasicShipperAction;
 
+import java.awt.*;
+
 public class BasicShipper extends Shipper {
 
-	BasicShipperAction actions;
+	private Dimension location;
+	private Package pack;
 
 	public BasicShipper(String name) {
 		super(name);
+	}
+
+	public Dimension getLocation() {
+		return location;
+	}
+
+	public void setLocation(Dimension location) {
+		this.location = location;
+	}
+
+	public Package getPack() {
+		return pack;
+	}
+
+	public void setPack(Package pack) {
+		this.pack = pack;
 	}
 }
