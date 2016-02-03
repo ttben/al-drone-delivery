d80036bde648d9152d0c704704992de0d6e33a71
Benjamin Benni <benjamin.benni06@gmail.com>
2016-02-03 19:10:51 +0100

    Add forgotten file of previous-last commit

--- newTruckApp/src/main/java/app/action/Collect.java
+++ newTruckApp/src/main/java/app/action/Collect.java
@@ -1,10 +1,11 @@
 package app.action;
 
+import app.shipper.BasicShipper;
 import app.shipper.CompositeShipper;
 
 public class Collect extends CompositeShipperAction {
 
 	public Collect(CompositeShipper target, Object... params) {
-		super(target, params);
+		super(target, (BasicShipper)params[0], params);
 	}
 }
--- newTruckApp/src/main/java/app/action/CompositeShipperAction.java
+++ newTruckApp/src/main/java/app/action/CompositeShipperAction.java
@@ -1,13 +1,21 @@
 package app.action;
 
+import app.shipper.BasicShipper;
 import app.shipper.CompositeShipper;
 
 public abstract class CompositeShipperAction extends Action {
 	private CompositeShipper target;
+	protected BasicShipper element;
 
-	public CompositeShipperAction(CompositeShipper target, Object ...params) {
+
+	public CompositeShipperAction(CompositeShipper target, BasicShipper element, Object ...params) {
 		super(params);
 		this.target = target;
+		this.element = element;
+	}
+
+	public BasicShipper getElement() {
+		return element;
 	}
 
 	@Override
